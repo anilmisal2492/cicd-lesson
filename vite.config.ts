11e222c165e0e1e8c647bb7d00f508fbccea7800
anilmisal2492 <anilmisal2492@gmail.com>
2026-09-09 08:50:14 +0000

    Fix GitHub Pages asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/test-day-7/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/cicd-lesson/' : '/',
 })
